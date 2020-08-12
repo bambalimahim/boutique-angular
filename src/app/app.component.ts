31849ba3592a3a7dd5ed5d173f950165c1fe1443
bamba <mouhamadoubamba.mboup@akilee-by-ines.com>
2020-08-12 12:48:52 +0000

    gestion bouditque done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'boutique';
+  constructor() {
+    var firebaseConfig = {
+      apiKey: "AIzaSyBkC2_J81zqt3ZpIdv5nQhInKX_zucwzwY",
+      authDomain: "boutique-angular.firebaseapp.com",
+      databaseURL: "https://boutique-angular.firebaseio.com",
+      projectId: "boutique-angular",
+      storageBucket: "boutique-angular.appspot.com",
+      messagingSenderId: "468493856723",
+      appId: "1:468493856723:web:518c97e9898fe0fafb4926",
+      measurementId: "G-FYK9RQJW4J"
+    };
+    firebase.initializeApp(firebaseConfig);
+  }
 }
